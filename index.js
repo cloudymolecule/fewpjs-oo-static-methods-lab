af1aa16c0e8c01815e3e14f38322c080dfdbc782
Cloudymolecule <baires82@gmail.com>
2021-03-02 15:46:05 -0500

    commit tree 89f105c47fcf98c0977570a5f4b67f7458571783

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 class Formatter {
-  //add static methods here
+
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '')
+  }
+
+  static titleize(string) {
+    const avoid = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    const arr = string.split(' ')
+    const newArr = []
+    arr.forEach(e => {
+      if (e === arr[0]) {
+        return newArr.push(Formatter.capitalize(e))
+      }
+      if (!avoid.find(el => el === e )){
+        return newArr.push(Formatter.capitalize(e))
+      } else {
+        return newArr.push(e)
+      }
+    })
+    return newArr.join(' ')
+  }
 }
